a7d9f01111279aecaf5a464f7c5c1d104d19539a
Sabbir Dewan <dwnsabbir@gmail.com>
2026-08-26 23:11:51 +0600

    updated

--- Module_1/function.js
+++ Module_1/function.js
@@ -32,11 +32,86 @@
 // console.log(spend, save);
 
 
-function sayHellow(name){                                      //; void function, it does not return any value. it just print the value of name variable.
+// function sayHellow(name){                                      //; void function, it does not return any value. it just print the value of name variable.
 
-    console.log(`Hellow ${name}`);
+//     console.log(`Hellow ${name}`);
+// }
+
+// sayHellow("dewan");
+
+
+
+// // 1 থেকে 10 পর্যন্ত শুধু 7 - এর চেয়ে বড় number print করো।
+
+// function printNumberGreaterThanSeven(number) {
+//     for (let i = 1; i <= number; i++) {
+//         if (i > 7) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// printNumberGreaterThanSeven(10);
+
+//একটা number 10-এর বেশি হলে "Big", না হলে "Small" return করো।
+
+// function checkNumber(a) {
+// if(a>10){return "big";}
+//     else{return "small";}}
+
+// console.log(checkNumber(16));
+
+//একটা number 0-এর বেশি হলে "Positive", 0-এর কম হলে "Negative", এবং 0 হলে "Zero" return করো।
+
+// function checkNumber(a){
+//     if(a>0) {return"positive";}
+// else if (a<0) {return "negative";}
+// else {return "zero";}}
+
+// console.log(checkNumber(10));
+
+//Even/Odd Checker 
+
+// function EvenNumber(a){
+// if (a%2===0) {return "even";}
+// else{return "odd";}
+// }
+
+// console.log(EvenNumber(10));
+
+
+//একটা number 7 দিয়ে নিঃশেষে ভাগ গেলে "Lucky", না হলে "Not Lucky" return করো।
+
+// function checknumber(a) {
+//     if (a % 7 === 0) {
+//         return "Lucky";
+//     } else {
+//         return "not lucky";
+//     }
+// }
+
+// console.log(checknumber(7));
+
+
+//একজন student-এর marks দেওয়া আছে।
+// 80 বা তার বেশি → "A"
+// 60 বা তার বেশি → "B"
+// 40 বা তার বেশি → "C"
+// 40 - এর কম → "Fail"
+
+
+function checkMarks(marks) {
+    if (marks >= 80) {
+        return "A";
+    } else if (marks >= 60) {
+        return "B";
+    } else if (marks >= 40) {
+        return "C";
+    } else {
+        return "Fail";
+    }
 }
 
-sayHellow("dewan");
+console.log(checkMarks(65));
 
 
